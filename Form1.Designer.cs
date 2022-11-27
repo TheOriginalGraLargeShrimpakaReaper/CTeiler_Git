d5bad30a15d02d932daf73c9fa339e908d8095f1
grabe <grabe@LAPTOP-S7IOFPB5>
2022-11-27 15:46:35 +0100

    Projektdateien hinzufügen.

---
+++ Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace CTeiler
+{
+    partial class CTeiler
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtEingabe = new System.Windows.Forms.TextBox();
+            this.lblEingabe = new System.Windows.Forms.Label();
+            this.lblResultat = new System.Windows.Forms.Label();
+            this.btnBerechne = new System.Windows.Forms.Button();
+            this.btnSchliessen = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtEingabe
+            // 
+            this.txtEingabe.Location = new System.Drawing.Point(150, 10);
+            this.txtEingabe.Name = "txtEingabe";
+            this.txtEingabe.Size = new System.Drawing.Size(100, 20);
+            this.txtEingabe.TabIndex = 0;
+            // 
+            // lblEingabe
+            // 
+            this.lblEingabe.Location = new System.Drawing.Point(15, 10);
+            this.lblEingabe.Name = "lblEingabe";
+            this.lblEingabe.Size = new System.Drawing.Size(100, 20);
+            this.lblEingabe.TabIndex = 1;
+            this.lblEingabe.Text = "Eingabe";
+            // 
+            // lblResultat
+            // 
+            this.lblResultat.AutoSize = true;
+            this.lblResultat.Location = new System.Drawing.Point(15, 35);
+            this.lblResultat.Name = "lblResultat";
+            this.lblResultat.Size = new System.Drawing.Size(0, 13);
+            this.lblResultat.TabIndex = 2;
+            // 
+            // btnBerechne
+            // 
+            this.btnBerechne.Location = new System.Drawing.Point(260, 10);
+            this.btnBerechne.Name = "btnBerechne";
+            this.btnBerechne.Size = new System.Drawing.Size(75, 20);
+            this.btnBerechne.TabIndex = 3;
+            this.btnBerechne.Text = "Berechnen";
+            this.btnBerechne.UseVisualStyleBackColor = true;
+            this.btnBerechne.Click += new System.EventHandler(this.btnBerechne_Click);
+            // 
+            // btnSchliessen
+            // 
+            this.btnSchliessen.Location = new System.Drawing.Point(260, 35);
+            this.btnSchliessen.Name = "btnSchliessen";
+            this.btnSchliessen.Size = new System.Drawing.Size(75, 20);
+            this.btnSchliessen.TabIndex = 4;
+            this.btnSchliessen.Text = "Schliessen";
+            this.btnSchliessen.UseVisualStyleBackColor = true;
+            this.btnSchliessen.Click += new System.EventHandler(this.btnSchliessen_Click);
+            // 
+            // CTeiler
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(334, 211);
+            this.Controls.Add(this.btnSchliessen);
+            this.Controls.Add(this.btnBerechne);
+            this.Controls.Add(this.lblResultat);
+            this.Controls.Add(this.lblEingabe);
+            this.Controls.Add(this.txtEingabe);
+            this.Name = "CTeiler";
+            this.RightToLeftLayout = true;
+            this.Text = "Teiler bestimmen";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtEingabe;
+        private System.Windows.Forms.Label lblEingabe;
+        private System.Windows.Forms.Label lblResultat;
+        private System.Windows.Forms.Button btnBerechne;
+        private System.Windows.Forms.Button btnSchliessen;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.VisualStyles;
+
+namespace CTeiler
+{
+    /// <summary>
+    /// Die Klasse CTeiler
+    /// Diese Erbt von der Klasse Form
+    /// </summary>
+    public partial class CTeiler : Form
+    {
+        //  Der Mini- und Maximale einzugebene Wert als Konstanten
+        public const int nMaxWert = 1000;
+        public const int nMinWert = 1;
+
+        /// <summary>
+        /// Der Konstruktor
+        /// </summary>
+        public CTeiler()
+        {
+            InitializeComponent();
+        }
+
+
+        /// <summary>
+        /// Schliesst die Applikation
+        /// Der User muss allerdings noch bestätigen
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnSchliessen_Click(object sender, EventArgs e)
+        {
+            DialogResult result = MessageBox.Show("Wollen Sie die Applikation wirklich schliessen?", "Schliessen", MessageBoxButtons.YesNo, MessageBoxIcon.Question, MessageBoxDefaultButton.Button1);
+            if (result == System.Windows.Forms.DialogResult.Yes)
+            {
+                MessageBox.Show("Applikation wird geschlossen");
+                System.Windows.Forms.Application.Exit();
+            }
+        }
+
+        /// <summary>
+        /// Führt die Berechnung aus
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnBerechne_Click(object sender, EventArgs e)
+        {
+            // Aufrufen der Methode ValidInput
+            (bool bValid, int nEingabe) = ValidInput(nMinWert, nMaxWert, txtEingabe);
+            if (!bValid)
+                return;
+
+            //  Teiler bestimmen lassen
+            string strTeilresultat = BestimmeTeiler(nEingabe, nMinWert, nMaxWert);
+
+            //  Resultat zurückgeben
+            SetzeResultat(strTeilresultat, lblResultat);
+        }
+
+        /// <summary>
+        /// Prüft die Eingabe auf folgendes
+        /// 1.  NOT NULL
+        /// 2.  Nicht Alphanummerisch
+        /// 3.  Ganzzahl
+        /// </summary>
+        /// <param name="nMinimumWert"></param>
+        /// <param name="nMaximumWert"></param>
+        /// <param name="oField"></param>
+        /// <returns></returns>
+        private (bool, int) ValidInput(int nMinimumWert, int nMaximumWert, TextBox oField)
+        {
+            //  Textfeld auslesen
+            string strEingabe = txtEingabe.Text.Trim();
+
+            //  Variablen deklarieren / definieren
+            int nEingabe = 0;
+            double dEingabe = 0.0;
+            string strWarningErroMessage = "";
+            bool bEingabe_Is_NULL = string.IsNullOrEmpty(strEingabe);               //  Prüfen ob der Wert NULL ist
+            bool bEingabe_Is_Parsable = int.TryParse(strEingabe, out nEingabe);     //  Prüfen ob in Integer geparsed werden kann
+            bool bEingabe_Is_Double = double.TryParse(strEingabe, out dEingabe);    //  Wird nur für dEingabe gebraucht
+            bool bValid = false;
+          
+            //  Prüfen ob kein Alphanummerischer Wert und kein NULL
+            if(bEingabe_Is_NULL == false && bEingabe_Is_Double)   //  Nummerisch und NOT NULL
+            {
+                //  Prüfen ob Nachkommastellen
+                if(dEingabe != ((int)dEingabe)) //  Keine Ganzzahl
+                {
+                    strWarningErroMessage = $"Ungültiger Wert: '{strEingabe}'.\rBitte nummerische, ganzzahligen Wert eingeben!";
+                    bValid = false;
+                }
+                else
+                {
+                    //  Prüfen, ob innerhalb der Range
+                    if(nEingabe >= nMinimumWert && nEingabe < nMaximumWert)    //  In der Range
+                    {
+                        bValid = true;
+                    }
+                    else                                                      //  Ausserhalb der Range
+                    {
+                        strWarningErroMessage = $"Ungültiger Wert: '{strEingabe}'.\rBitte nummerische, ganzzahligen Wert eingeben!";
+                        bValid =false;
+                    }
+                }
+            }
+            else    //  Alphanummerisch oder NULL
+            {
+                //  Prüfen, was genau falsch ist
+                if(bEingabe_Is_NULL)            //  NULL Value
+                    strWarningErroMessage = $"Eingabe leer!\rBitte nummerische, ganzzahligen Wert eingeben!";
+                else
+                    if(!bEingabe_Is_Parsable)   //    Alphanummerischer Wert
+                        strWarningErroMessage = $"Ungültiger Wert: '{strEingabe}'.\rBitte nummerische, ganzzahligen Wert eingeben!";
+
+                //  In jedem Fall ist die Validität false
+                bValid = false;
+            }
+
+            //  Warnung / Error Ausgeben
+            if(bValid == false)
+            {
+                WarningErrorMessage(strWarningErroMessage, oField);
+            }
+
+            //  Zürückgeben der Validität und Eingabewertes als Integer
+            return (bValid, nEingabe);
+        }
+
+        /// <summary>
+        /// Gibt erst die Fehlermeldung aus und leert danach das Eingabefeld und legt den Focus daraus
+        /// </summary>
+        /// <param name="strMessage"></param>
+        /// <param name="oField"></param>
+        private void WarningErrorMessage(string strMessage, TextBox oField)
+        {
+            MessageBox.Show(strMessage, "Benutzereingabe", MessageBoxButtons.OK, MessageBoxIcon.Error, MessageBoxDefaultButton.Button1);
+            oField.Clear();
+            oField.Focus();
+        }
+
+        /// <summary>
+        /// Bestimmt den Teiler des Übergebenen Wertes.
+        /// Als String werden alle Werte mit einem Einleitungstext und die Resultate dann
+        /// mit Zeilenumbruch zurückgegeben
+        /// </summary>
+        /// <param name="nWert"></param>
+        /// <param name="nMinimumWert"></param>
+        /// <param name="nMaximumWert"></param>
+        /// <returns></returns>
+        private string BestimmeTeiler(int nWert, int nMinimumWert, int nMaximumWert)
+        {
+            //  Den Rückgabe String deklarieren und mit dem Text bereits definieren
+            string strWert = $"Die ganzzahligen Teiler von {nWert} sind:";
+
+            //  Counter setzen
+            /*
+             * Hier war angedacht gewesen, das ganze zu optimieren.
+             * Doch da auch ungerade Werte vorkommen können (Teiler von 30 ist auch 3)
+            */
+            int nCounter = 1;
+
+            //  Durchlaufen und Teiler bestimmen.
+            for (int nLaufnummer = nMinimumWert; nLaufnummer < nWert; nLaufnummer = nLaufnummer + nCounter)
+            {
+                //  Wenn Modulo 0 zurückgibt ist es ein Teiler
+                if (nWert % nLaufnummer == 0)
+                {
+                    string cTeiler = nLaufnummer.ToString();
+                    strWert = strWert + Environment.NewLine + cTeiler;
+                }
+            }
+
+            //  Konkatinierter String zurückgeben
+            return strWert;
+        }
+
+        /// <summary>
+        /// Wert auf das Übergebene Label setzen
+        /// </summary>
+        /// <param name="strWert"></param>
+        /// <param name="oLabel"></param>
+        private void SetzeResultat(string strWert, Label oLabel)
+        {
+            oLabel.Text = strWert;
+        }
+
+    }
+}
